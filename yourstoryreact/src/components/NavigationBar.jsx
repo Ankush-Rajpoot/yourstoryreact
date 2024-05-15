32a9de76ca0c2021d710423f0e6d6c2b533a63c7
Nakesh_Tewari <nakesh1107@gmail.com>
2024-05-15 22:30:42 +0530

    Made LOgin and SignUp Pages

--- yourstoryreact/src/components/NavigationBar.jsx
+++ yourstoryreact/src/components/NavigationBar.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import CategoriesWrite from './CategoriesWrite'; // Updated import
-import CategoriesRead from './CategoriesRead'; // Updated import
+import CategoriesWrite from "./CategoriesWrite"; // Updated import
+import CategoriesRead from "./CategoriesRead"; // Updated import
+import { Link } from "react-router-dom";
 
 import logo from "../assets/yourstoryimages/logo.png";
 import logo1 from "../assets/yourstoryimages/logo1.png";
 
 const NavigationBar = () => {
-    const [modalIsOpenWrite, setModalIsOpenWrite] = useState(false);
-    const [modalIsOpenRead, setModalIsOpenRead] = useState(false);
+  const [modalIsOpenWrite, setModalIsOpenWrite] = useState(false);
+  const [modalIsOpenRead, setModalIsOpenRead] = useState(false);
 
-    const handleOpenModalWrite = () => {
-        setModalIsOpenWrite(true);
-    };
-    const handleCloseModalWrite = () => {
-        setModalIsOpenWrite(false);
-    };
-    const handleOpenModalRead = () => {
-        setModalIsOpenRead(true);
-    };
-    const handleCloseModalRead = () => {
-        setModalIsOpenRead(false);
-    };
-    
+  const handleOpenModalWrite = () => {
+    setModalIsOpenWrite(true);
+  };
+  const handleCloseModalWrite = () => {
+    setModalIsOpenWrite(false);
+  };
+  const handleOpenModalRead = () => {
+    setModalIsOpenRead(true);
+  };
+  const handleCloseModalRead = () => {
+    setModalIsOpenRead(false);
+  };
 
   useGSAP(() => {
     var tl = gsap.timeline();
@@ -42,37 +42,56 @@ const NavigationBar = () => {
     });
   });
 
-    return (
-        <>
-            <div id="logo-container">
-                <img src={logo1} alt="Logo" />
-            </div>
-            <div className='navContent' id='content'>
-                <nav>
-                    <div className="logo">
-                        <img src={logo} alt="YourStory Logo" />
-                    </div>
-                    <ul>
-                        <li>
-                            <button id="AboutUs">About</button>
-                        </li>
-                        <li>
-                            <button id='Write' onClick={handleOpenModalWrite}>Write</button>
-                        </li>
-                        <li>
-                            <button id='Read' onClick={handleOpenModalRead}>Read</button>
-                        </li>
-                        <li>
-                            <button id="Login/SignUp">Login/SignUp</button>
-                        </li>
-                        <li className="profile"></li>
-                    </ul>
-                </nav>
-            </div>
-            <CategoriesWrite open={modalIsOpenWrite} handleCloseModalWrite={handleCloseModalWrite} /> {/* Updated usage */}
-            <CategoriesRead open={modalIsOpenRead} handleCloseModalRead={handleCloseModalRead} /> {/* Updated usage */}
-        </>
-    );
+  return (
+    <>
+      <div id="logo-container">
+        <img src={logo1} alt="Logo" />
+      </div>
+      <div className="navContent" id="content">
+        <nav>
+          <div className="logo">
+            <img src={logo} alt="YourStory Logo" />
+          </div>
+          <ul>
+            <li>
+              <button id="AboutUs">About</button>
+            </li>
+            <li>
+              <button id="Write" onClick={handleOpenModalWrite}>
+                Write
+              </button>
+            </li>
+            <li>
+              <button id="Read" onClick={handleOpenModalRead}>
+                Read
+              </button>
+            </li>
+            <li>
+              <button id="Login/SignUp">
+                <Link to="/LoginPage">Login</Link>
+              </button>
+            </li>
+            <li>
+              <button id="Login/SignUp">
+                <Link to="SignUpPage">SignUp</Link>
+              </button>
+            </li>
+            <li className="profile"></li>
+          </ul>
+        </nav>
+      </div>
+      <CategoriesWrite
+        open={modalIsOpenWrite}
+        handleCloseModalWrite={handleCloseModalWrite}
+      />{" "}
+      {/* Updated usage */}
+      <CategoriesRead
+        open={modalIsOpenRead}
+        handleCloseModalRead={handleCloseModalRead}
+      />{" "}
+      {/* Updated usage */}
+    </>
+  );
 };
 
 export default NavigationBar;
--- yourstoryreact/src/components/SignUp.jsx
+++ yourstoryreact/src/components/SignUp.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-export default function LoginPage() {
+export default function SignUp() {
   return (
     <form action="" className="LoginPage">
-      <h1>Login Page</h1>
+      <h1>SignUp Page</h1>
       <label htmlFor="">Enter Name </label>
       <input type="text" />
       <br />
